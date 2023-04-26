9fb7386cb800c5590a73602c507eecf7f8ad95f2
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-26 16:20:45 +0530

    add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='nowpy',
+    version='0.2.0',
+    packages=[
+        'nowpy'
+    ],
+    install_requires=[
+        'cbor>=1.0.0,<2.0.0',
+        'pyserial>=3.5.0,<4.0.0'
+    ],
+    extras_require={
+        'ble': [
+            'bleak==0.13.0',
+            'aioitertools==0.8.0'
+        ],
+        'requests': [
+            'requests>=2.26.0,<3.0.0'
+        ]
+    }
+)
