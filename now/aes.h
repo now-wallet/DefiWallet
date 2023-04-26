29983d4c35f4b5dd03cce6b2599d5b598f7fe4ec
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-26 16:30:18 +0530

    aes: encrypt + decrypt bytes

--- now/aes.h
+++ now/aes.h
@@ -1,14 +1,55 @@
-#pragma once
+/**
+ * @file aes.h
+ * @author Krisna Pranav
+ * @brief AES Encryption
+ * @version 0.1
+ * @date 2023-04-26
+ * 
+ * @copyright Copyright (c) 2023 Krisna Pranav. NowWallet
+ * 
+ */
+
+#ifndef AES_H
+#define AES_H
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <wally_crypto.h>
 
-#define AES_PADDED_LEN(len) (((len / AES_BLOCK_LEN ) + 1) * AES_BLOCK_LEN)
+/**
+ * @brief AES PADDED LEN + AES ENCRYPTED LEN
+ * 
+ */
+#define AES_PADDED_LEN(len) (((len / AES_BLOCK_LEN) + 1) * AES_BLOCK_LEN)
 #define AES_ENCRYPTED_LEN(len) (AES_BLOCK_LEN + AES_PADDED_LEN(len))
 
 /**
- * @brief aes encrypted bytes
+ * @brief aes encrypt bytes
  * 
+ * @param aeskey 
+ * @param aeskey_len 
+ * @param bytes 
+ * @param bytes_len 
+ * @param output 
+ * @param output_len 
+ * @return true 
+ * @return false 
  */
-bool aes_encrypt_bytes(const uint8_t* aeskey, size_t asekey_len, const uint8_t* bytes, size_t bytes_len, uint8_t* output, size_t output_len)
+bool aes_encrypt_bytes(const uint8_t* aeskey, size_t aeskey_len, const uint8_t* bytes, size_t bytes_len, uint8_t* output, size_t output_len);
+
+/**
+ * @brief aes decrypt bytes
+ * 
+ * @param aeskey 
+ * @param aeskey_len 
+ * @param bytes 
+ * @param bytes_len 
+ * @param output 
+ * @param output_len 
+ * @param written 
+ * @return true 
+ * @return false 
+ */
+bool aes_decrypt_bytes(const uint8_t* aeskey, size_t aeskey_len, const uint8_t* bytes, size_t bytes_len, uint8_t* output, size_t output_len, size_t* written);
+
+#endif 
