78ee4a3669adc50579d9e91ec928363a6bfe316b
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-26 17:02:23 +0530

    nowpy: now_error

--- nowpy/now_error.py
+++ nowpy/now_error.py
@@ -3,15 +3,19 @@ class NowError(Exception):
     UNKNOWN_METHOD = -32601
     BAD_PARAMETERS = -32602
     INTERNAL_ERROR = -32603
+
     USER_CANCELLED = -32000
     PROTOCOL_ERROR = -32001
     HW_LOCKED = -32002
     NETWORK_MISMATCH = -32003
-    
+
     def __init__(self, code, message, data):
         self.code = code
         self.message = message
         self.data = data
+
+    def __repr__(self):
+        return "NowError: " + str(self.code) + " - " + self.message  + " (Data: " + repr(self.data) + ")"
 
     def __str__(self):
         return repr(self)
