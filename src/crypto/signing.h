a04ca35226ac27f22052b43377ce715b64ced29f
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-27 17:36:04 +0530

    crypto: signing

--- src/crypto/signing.h
+++ src/crypto/signing.h
@@ -15,15 +15,22 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <ox.h>
+#include <os.h>
 #include <cx.h>
+
 #include "src/consts.h"
 
+/**
+ * @brief signing context
+ * 
+ */
 typedef struct signing_context_t {
-    cx_curve_t curve;
-    uint8_t pathLength;
-    uint32_t bip32Path[ADDRESS_MAX_BIP32_PATH];
-    bool isSchnorr;
+    cx_curve_t  curve;
+    uint8_t     pathLength;
+    uint32_t    bip32Path[ADDRESS_MAX_BIP32_PATH];
+    uint8_t     data[MAX_RAW_OPERATION];
+    uint32_t    dataLength;
+    bool        isSchnorr;
 } SigningContext;
 
 /**
@@ -32,8 +39,24 @@ typedef struct signing_context_t {
  * @param privateKey 
  * @param hash 
  * @param signature 
+ * @param signatureSize 
  * @return uint32_t 
  */
-uint32_t signEcdsa(const cx_ecfp_private_key_t *privateKey, const uint8_t *hash, uint8_t *signature);
+uint32_t signEcdsa(const cx_ecfp_private_key_t *privateKey,
+                   const uint8_t *hash,
+                   uint8_t *signature,
+                   size_t signatureSize);
 
-#endif
+/**
+ * @brief sign schnorr
+ * 
+ * @param privateKey 
+ * @param hash 
+ * @param signature 
+ * @return uint32_t 
+ */
+uint32_t signSchnorr(const cx_ecfp_private_key_t *privateKey,
+                     const uint8_t *hash,
+                     uint8_t *signature);
+
+#endif 
