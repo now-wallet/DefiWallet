e0faacb4d7003748cee7de46d00b1cb8ec6afb8f
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-27 15:20:13 +0530

    utils: unpack

--- src/utils/unpack.h
+++ src/utils/unpack.h
@@ -21,32 +21,56 @@
 #endif
 
 /**
- * @brief U1SIZE
+ * @brief USIZES[1,2,4,8]
  * 
  */
-static const size_t U1_SIZE = sizeof(uint8_t);
+static const size_t U1_SIZE         = sizeof(uint8_t);
+static const size_t U2_SIZE         = sizeof(uint16_t);
+static const size_t U4_SIZE         = sizeof(uint32_t);
+static const size_t U8_SIZE         = sizeof(uint64_t);
 
 /**
- * @brief U2SIZE
+ * @brief UMAX[1,2,4,8]
  * 
  */
-static const size_t U2_SIZE = sizeof(uint16_t);
+static const uint8_t U1_MAX         = 0xFFU;
+static const uint16_t U2_MAX        = 0xFFFFUL;
+static const uint32_t U4_MAX        = 0xFFFFFFFFUL;
+static const uint64_t U8_MAX        = 0xFFFFFFFFFFFFFFFFULL;
 
 /**
- * @brief U4SIZE
+ * @brief USHIFT[1,2,4]
  * 
  */
-static const size_t U4_SIZE = sizeof(uint32_t);
+static const uint16_t U1_SHIFT      = U1_SIZE * U8_SIZE;
+static const uint32_t U2_SHIFT      = U2_SIZE * U8_SIZE;
+static const uint64_t U4_SHIFT      = U4_SIZE * U8_SIZE;
 
 /**
- * @brief U8SIZE
+ * @brief U2LE
  * 
  */
-static const size_t U8_SIZE = sizeof(uint64_t);
-
 #define U2LE(src, offset)                                                   \
     ((uint16_t)((src)[(offset)]                 & U1_MAX)               |   \
     ((uint16_t)((src)[(offset) + U1_SIZE]       & U1_MAX)   << U1_SHIFT)    \
                                                 & U2_MAX)
 
-#endif
+/**
+ * @brief U4LE
+ * 
+ */
+#define U4LE(src, offset)                                                   \
+    ((uint32_t)(U2LE((src), (offset))           & U2_MAX)               |   \
+    ((uint32_t)(U2LE((src), (offset) + U2_SIZE) & U2_MAX)   << U2_SHIFT)    \
+                                                & U4_MAX)
+
+/**
+ * @brief U8LE
+ * 
+ */
+#define U8LE(src, offset)                                                   \
+    ((uint64_t)(U4LE((src), (offset))           & U4_MAX)               |   \
+    ((uint64_t)(U4LE((src), (offset) + U4_SIZE) & U4_MAX)   << U4_SHIFT)    \
+                                                & U8_MAX)
+
+#endif  
