5a4a9f75d8b92cc571700f785e842bd5bcf723c9
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-27 17:35:13 +0530

    crypto: signing

---
+++ src/crypto/signing.h
@@ -0,0 +1,39 @@
+/**
+ * @file signing.h
+ * @author Krisna Pranav
+ * @brief Signing
+ * @version 0.1
+ * @date 2023-04-27
+ * 
+ * @copyright Copyright (c) 2023 Krisna Pranav, NowWallet
+ * 
+ */
+
+#ifndef NOW_CRYPTO_SIGNING_H
+#define NOW_CRYPTO_SIGNING_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <ox.h>
+#include <cx.h>
+#include "src/consts.h"
+
+typedef struct signing_context_t {
+    cx_curve_t curve;
+    uint8_t pathLength;
+    uint32_t bip32Path[ADDRESS_MAX_BIP32_PATH];
+    bool isSchnorr;
+} SigningContext;
+
+/**
+ * @brief sign ecdsa
+ * 
+ * @param privateKey 
+ * @param hash 
+ * @param signature 
+ * @return uint32_t 
+ */
+uint32_t signEcdsa(const cx_ecfp_private_key_t *privateKey, const uint8_t *hash, uint8_t *signature);
+
+#endif
