c1719f99ac19e183a7c9563204880a94d03e6742
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-27 16:38:10 +0530

    crypto: key

--- src/crypto/key.c
+++ src/crypto/key.c
@@ -5,4 +5,20 @@
 //  Created by krisna pranav on 27/04/23.
 //
 
-#include "key.h"
+#include <stddef.h>
+#include <stdint.h>
+#include "crypto/key.h"
+#include "src/consts.h"
+#include "utils/util.h"
+
+size_t compressPublicKey(const uint8_t *uncompressed, uint8_t *compressed) {
+    if (uncompressed == NULL || compressed == NULL) {
+        return 0;
+    }
+    
+    const uint8_t EC_PUBLICKEY_EVEN_PREFIX = 0x02;
+    const uint8_t EC_PUBLICKEY_ODD_PREFIX = 0x03;
+    const uint8_t EC_PUBLICKEY_UNCOMPRESSED_PREFIX = 0x04;
+
+    return EC_PUBLICKEY_UNCOMPRESSED_PREFIX;
+}
